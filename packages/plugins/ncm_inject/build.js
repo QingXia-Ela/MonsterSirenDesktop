c54f759b3347b0743d8afc8437f49cd43958309d
QingXia-Ela <Elahere@163.com>
2023-12-30 17:33:07 +0800

    chore: 增加浏览器打包配置

--- packages/plugins/ncm_inject/build.js
+++ packages/plugins/ncm_inject/build.js
@@ -1,4 +1,9 @@
 import * as esbuild from 'esbuild'
+import * as vite from 'vite'
+
+const logger = vite.createLogger("info", {
+  prefix: "[ncm_inject]",
+})
 
 // bundle node
 await esbuild.build({
@@ -14,4 +19,19 @@ await esbuild.build({
   minify: true,
 })
 
-// bundle browser
+logger.info('build node code ./dist/bundle.cjs success')
+
+// bundle browser settings render
+esbuild.build({
+  entryPoints: ['./src/index.ts'],
+  outfile: './dist-browser/index.js',
+  platform: 'browser',
+  target: 'chrome105',
+  format: 'cjs',
+  define: {
+    'process.env.NODE_ENV': '"production"'
+  },
+  minify: true,
+})
+
+logger.info('build browser settings code ./dist-browser/index.js success')
--- packages/plugins/ncm_inject/vite.config.ts
+++ packages/plugins/ncm_inject/vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   build: {
     // Tauri supports es2021
     // In current app we only support windows, which is target to be chrome105
     target: "chrome105",
+    outDir: "dist-html",
   },
 })
