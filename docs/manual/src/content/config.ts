78f54b68643d3af25d9b015aaf39d8f2eca63166
QingXia-Ela <Elahere@163.com>
2024-08-21 22:03:25 +0800

    docs: 修复文档错误问题

--- docs/manual/src/content/config.ts
+++ docs/manual/src/content/config.ts
@@ -1,7 +1,5 @@
 import { defineCollection, z } from 'astro:content';
+import { docsSchema } from '@astrojs/starlight/schema';
 export const collections = {
-  'docs': defineCollection({
-    type: 'content',
-    schema: z.any()
-  }),
+  'docs': docsSchema(),
 };
